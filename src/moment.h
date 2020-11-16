740134d2c32961f8b2c647be0aeef196e1f7fe5d
Leonardo <imrealleonardo@gmail.com>
2020-11-16 17:23:58 +0800

    -init project;

---
+++ src/moment.h
@@ -0,0 +1,9 @@
+#ifndef MOMENT_SRC_MOMENT_H
+#define MOMENT_SRC_MOMENT_H
+
+/*
+ * 这个文件是干嘛的？
+ * 定义moment类？
+ */
+
+#endif //MOMENT_SRC_MOMENT_H
---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * 在这里写测试用例
+ */
+
+#include "../src/moment.h"
+
+int main() {
+
+  return 0;
+}
